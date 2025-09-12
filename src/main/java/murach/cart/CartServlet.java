a44aea875cadbe0a9f78af69fbba72d580fad199
Trantien2005 <tranthaotien2k5@gmail.com>
2025-09-12 15:55:47 +0700

    Chương 7+9

---
+++ src/main/java/murach/cart/CartServlet.java
@@ -0,0 +1,78 @@
+package murach.cart;
+
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+
+import murach.business.*;
+import murach.data.ProductIO;
+
+public class CartServlet extends HttpServlet {
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        ServletContext sc = getServletContext();
+        String action = request.getParameter("action");
+        if (action == null) {
+            action = "cart";
+        }
+
+        String url = "/index.jsp";
+        HttpSession session = request.getSession();
+        Cart cart = (Cart) session.getAttribute("cart");
+        if (cart == null) {
+            cart = new Cart();
+        }
+
+        if (action.equals("shop")) {
+            url = "/index.jsp";
+        } else if (action.equals("cart")) {
+        	String mode = request.getParameter("mode");
+            String productCode = request.getParameter("productCode");
+            String quantityString = request.getParameter("quantity");
+            int quantity = 1;  // default
+            if (productCode == null || productCode.isEmpty()) {
+                response.sendRedirect("index.jsp");
+                return;
+            }
+
+            try {
+                quantity = Integer.parseInt(quantityString);
+                if (quantity < 0) quantity = 1;
+            } 
+            catch (NumberFormatException e) {
+                quantity = 1;
+            }
+            String path = sc.getRealPath("/WEB-INF/products.txt");
+            Product product = ProductIO.getProduct(productCode, path);
+
+            LineItem item = new LineItem();
+            item.setProduct(product);
+            item.setQuantity(quantity);
+            if (quantity > 0) {
+            	if("update".equals(mode)) {
+                	cart.updateItem(item);
+                }
+            	else {
+                cart.addItem(item);
+            }
+            } else {
+                cart.removeItem(item);
+            }
+            
+            session.setAttribute("cart", cart);
+            url = "/cart.jsp";
+        } else if (action.equals("checkout")) {
+            url = "/checkout.jsp";
+        }
+
+        sc.getRequestDispatcher(url).forward(request, response);
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        doPost(request, response);
+    }
+}
